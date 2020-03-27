ad5318cdead6e5d5b7e2d9b2b8ce0cd3533bbb1e
Timo-Pekka Koljonen <holjis36@live.com>
2020-03-27 10:47:17 +0200

    Test data added

--- src/components/Kulukortti/Kulukortti.js
+++ src/components/Kulukortti/Kulukortti.js
@@ -1,21 +1,36 @@
 import React from 'react';
+import moment from 'moment';
+
 
 import './Kulukortti.css';
 
 function Kulukortti(props) {
+
+    let maksupaiva = moment(props.data.maksupaiva);
+    let kausi = "";
+    let keskiarvo;
+
+    if (props.data.kaudenalku && props.data.kaudenloppu) {
+      let kaudenalku = moment(props.data.kaudenalku);
+      let kaudenloppu = moment(props.data.kaudenloppu);
+      kausi = kaudenalku.format("D.M.Y") + " - " + kaudenloppu.format("D.M.Y");
+      let paivat = kaudenloppu.diff(kaudenalku, 'days');
+      keskiarvo = props.data.summa / paivat * 30;
+    }
+
     return (
       <div className="kulukortti">
         <div className="kulukortti__rivi">
-          <div className="kulukortti__tyyppi">Sähkö</div>
-          <div className="kulukortti__summa">437,50 €</div>
+          <div className="kulukortti__tyyppi">{props.data.tyyppi}</div>
+          <div className="kulukortti__summa">{props.data.summa.toFixed(2)} €</div>
         </div>
         <div className="kulukortti__rivi">
-          <div className="kulukortti__maksupaiva">20.3.2019</div>
-          <div className="kulukortti__ajanjakso">1.12.2018-28.2.2019</div>
+          <div className="kulukortti__maksupaiva">{maksupaiva.format("D.M.Y")}</div>
+          <div className="kulukortti__ajanjakso">{kausi}</div>
         </div>
         <div className="kulukortti__rivi">
-          <div className="kulukortti__laskuttaja">Fortum</div>
-          <div className="kulukortti__keskiarvo">145,83 € / kk</div>
+          <div className="kulukortti__laskuttaja">{props.data.saaja}</div>
+          <div className="kulukortti__keskiarvo">{ keskiarvo ? keskiarvo.toFixed(2) + " € / kk" : ""}</div>
         </div>
       </div>
     );
